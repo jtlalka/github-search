c6390cf69c0077c8cf9f62d62bf15fdb1670dfbf
Jakub Tlalka <jakub@tlalka.net>
2019-10-16 07:48:05 +0200

    Add progress bar for fetching data

--- app/src/main/kotlin/net/tlalka/github/search/feature/projectlist/view/binding/ProjectListBindingAdapter.kt
+++ app/src/main/kotlin/net/tlalka/github/search/feature/projectlist/view/binding/ProjectListBindingAdapter.kt
@@ -1,10 +1,28 @@
 package net.tlalka.github.search.feature.projectlist.view.binding
 
+import android.view.View
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import com.google.android.material.card.MaterialCardView
 import net.tlalka.github.search.R
 
 object ProjectListBindingAdapter {
+
+    @JvmStatic
+    @BindingAdapter("custom_toggle")
+    fun View.toggle(isToggle: Boolean?) {
+        visibility = if (isToggle == true) {
+            View.VISIBLE
+        } else {
+            View.GONE
+        }
+    }
+
+    @JvmStatic
+    @BindingAdapter("custom_projectItemMarker")
+    fun MaterialCardView.projectItemMarker(isSelected: Boolean) {
+        isActivated = isSelected
+    }
 
     @JvmStatic
     @BindingAdapter("custom_projectSizeInMb")
